5e515e9edba7d8350d8341f3f7f30632ede1212e
Bee28Gabz <gabrielgranera28@hotmail.com>
2024-06-30 04:32:27 -0600

    Configuraciones y base de datos

---
+++ Practica03/src/main/java/com/practica03/service/FirebaseStorageService.java
@@ -0,0 +1,20 @@
+package com.practica03.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "practica03-eb815.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "practica03";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "practica03-eb815-firebase-adminsdk-ao944-5ff255d81c.json";
+}
